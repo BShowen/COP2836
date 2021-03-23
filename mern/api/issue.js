433304c85d5a3dc992fd7bea392e68d1cfe30419
Bradley Showen <bshowen@live.seminolestate.edu>
2021-03-22 20:24:48 -0400

    Completed section

--- mern/api/issue.js
+++ mern/api/issue.js
@@ -2,9 +2,11 @@ const { getDb, getNextSequence } = require('./db.js');
 
 const { UserInputError } = require('apollo-server-express');
 
-async function list() {
+async function list(_, { status }) {
   const db = getDb();
-  const issues = await db.collection('issues').find({}).toArray();
+  const filter = {};
+  if (status) filter.status = status;
+  const issues = await db.collection('issues').find(filter).toArray();
   return issues;
 }
 
