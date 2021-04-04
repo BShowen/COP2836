fe2f86d40cbb93f9e5292c5d8e5435d761a2b849
Bradley Showen <bshowen@live.seminolestate.edu>
2021-04-04 15:45:02 -0400

    Controlled components in forms - Done

--- mern/ui/src/IssueFilter.jsx
+++ mern/ui/src/IssueFilter.jsx
@@ -4,34 +4,70 @@ import URLSearchParams from 'url-search-params';
 import { withRouter } from 'react-router-dom';
 
 class IssueFilter extends React.Component {
-  constructor(){
+  constructor({ location: { search } }){
     super();
+    const params = new URLSearchParams(search);
+    this.state = {
+      status: params.get('status') || '',
+      changed: false,
+    }
     this.onChangeStatus = this.onChangeStatus.bind(this);
+    this.applyFilter = this.applyFilter.bind(this);
+    this.showOriginalFilter = this.showOriginalFilter.bind(this);
   }
 
-  onChangeStatus(e){
-    const status = e.target.value;
+  applyFilter(){
+    const { status } = this.state;
     const { history } = this.props;
     history.push({
       pathname: '/issues', 
-      search: status ? `?status=${status}` : '', 
+      search: status ? `?status=${status}` : '',
+    });
+  }
+
+  componentDidUpdate(prevProps){
+    const { location: { search: prevSearch } } = prevProps;
+    const { location: { search } } = this.props;
+    if (prevSearch !== search){
+      this.showOriginalFilter();
+    }
+  }
+
+  onChangeStatus(e){
+    this.setState({ 
+      status: e.target.value, 
+      changed: true 
+    });
+  }
+
+  showOriginalFilter(){
+    const { location: { search } } = this.props;
+    const params = new URLSearchParams(search);
+    this.setState({
+      status: params.get('status') || '',
+      changed: false,
     });
   }
 
   render(){
-    const { location: { search } } = this.props;
-    const params = new URLSearchParams(search);
+    const { status, changed } = this.state;
+    // const { location: { search } } = this.props;
+    // const params = new URLSearchParams(search);
     return (
       <div>
         Status: 
         {' '}
-        <select value={ params.get('status') || '' } onChange={this.onChangeStatus}>
+        <select value={ status } onChange={this.onChangeStatus}>
           <option value="">(All)</option>
           <option value="New">New</option>
           <option value="Assigned">Assigned</option>
           <option value="Fixed">Fixed</option>
           <option value="Closed">Closed</option>
         </select>
+        {' '}
+        <button type='button' onClick={this.applyFilter}>Apply</button>
+        {' '}
+        <button type='button' onClick={this.showOriginalFilter} disabled={!changed}>Reset</button>
       </div>
     );
   }
